b15d18d71feaa401a3af02d9f5fd82fbf76a5208
ITE03050230 <karta2208100@kimo.com>
2016-09-19 05:19:32 -0400

    iuhuihiuhui jiji

---
+++ Lab03.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	int n1,n2,n3,n4,n5,n6,n7,n8,n9,n10;
+	printf("Please key ten number");
+	scanf("%d %d %d %d %d %d %d %d %d %d",&n1,&n2,&n3,&n4,&n5,&n6,&n7,&n8,&n9,&n10);
+	printf("Your numbers:\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n",n1,n2,n3,n4,n5,n6,n7,n8,n9,n10);
+	system("pause");	
+	return 0;
+}
